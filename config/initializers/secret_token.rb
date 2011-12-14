35e937ee0cb6c0fa089e8bcdd0053938c4c1ceec
frecaze <frecaze@frecaze-1215P.(none)>
2011-12-13 19:51:17 -0500

    OMG mi primer Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomeApp::Application.config.secret_token = '3b4a7e68ed09b10aea7ddc856aae134e38e29278c99dcb7ce1421d7b907d5c7e41bed242465c35b5cfefd6a266fa6d6e3b10b6e2f622b1314d186f00a0a5f059'
